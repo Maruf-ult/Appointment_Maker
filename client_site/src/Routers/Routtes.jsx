e67da3001100d1a04fbdcdb43e3490dc10651fae
maruf_hossain <marufhossain123098@gmail.com>
2024-11-20 23:50:31 +0600

    user and doc page added

--- client_site/src/Routers/Routtes.jsx
+++ client_site/src/Routers/Routtes.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router , Routes ,Route } from "react-router-dom"
 import Login from "../Login/Login.jsx"
 import Home from "../Home/Home.jsx"
 import Register from "../Login/Register.jsx"
+import ForClients from "../components/ForClients.jsx"
+import ForDoctors from "../components/ForDoctors.jsx"
+
 
 function Routtes() {
   return (
@@ -10,7 +13,9 @@ function Routtes() {
              <Routes>
                    <Route path='/' element={<Home></Home>}></Route>
                    <Route path='/login' element={<Login></Login>}></Route>
-                   <Route path="/register" element={<Register></Register>}/>
+                   <Route path="/reg" element={<Register></Register>}/>
+                   <Route path="/user" element={<ForClients></ForClients>}/>
+                   <Route path="/doc" element={<ForDoctors></ForDoctors>}/>
              </Routes>
          </Router>
     </>
---
+++ client_site/src/components/ForDoctors.jsx
@@ -0,0 +1,41 @@
+
+
+function ForDoctors() {
+     return (
+       <>
+       <div className="h-screen w-screen bg-slate-300 flex ">
+         
+         
+         
+         <div className="h-full  w-48 bg-slate-500 ml-6  mt-1   rounded-md ">
+          <div className="flex flex-col space-y-24  ">
+              <h1 className="text-3xl font-bold ml-3 mt-2" >SH <br /> <span className="text-2xl font-normal">Doctor</span> </h1>
+               <ul className="space-y-6 ml-16 text-white ">
+               <li className="cursor-pointer hover:text-cyan-900 font-bold ">Home</li>
+                 <li className="cursor-pointer hover:text-cyan-900 font-bold ">Doctors</li>
+                 <li className="cursor-pointer hover:text-cyan-900 font-bold ">Profile</li>
+                 <li className="cursor-pointer hover:text-cyan-900 font-bold ">Logout</li>
+               </ul>
+          </div>
+          </div>
+   
+         
+         <div className="bg-slate-300 h-screen w-screen  ml-5  rounded-md mr-4">
+   
+           <div  className="flex bg-slate-500 p-5 rounded-md justify-end mr-3 ml-3 mt-2 text-white font-bold ">
+               Doctor
+           </div>
+           <div className="bg-slate-50 mr-3 ml-3 mt-5 h-full rounded-md">
+              mamfadsf
+           </div>
+           
+         </div>
+   
+   
+   
+          </div>
+       </>
+     )
+   }
+   
+   export default ForDoctors;
