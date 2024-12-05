41c9589493b663c615c157bce610cd024d9c8c25
maruf_hossain <marufhossain123098@gmail.com>
2024-12-05 22:19:57 +0600

    apply doc schema upadated

--- server_site/usermodel/doctorSchema.js
+++ server_site/usermodel/doctorSchema.js
@@ -1,57 +1,47 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 const docSchema = new mongoose.Schema({
-     firstName:{
-          type:String,
-          required:true
-     },
-     lastName:{
-          type:String,
-          required:true
-     },
-     email:{
-          type:String,
-          required:true,
-     },
-     phoneNumber:{
-          type:String,
-          required:true,
-     },
-     website:{
-          type:String,
-          required:true
-     },
-     address:{
-          type:String,
-          required:true
-     },
-     specialization:{
-          type:String,
-          required:true,
-     },
-     experience:{
-          type:String,
-          required:true,
-     },
-     feeperCunsultation:{
-          type:Number,
-          required:true,
-     },
-     consultationHours:{
-          type:Number,
-          required:true,
-     },
-     timings:{
-          type:Array,
-          required:true
-     },
-     status:{
-          type:String,
-          default:"pending",
-     }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  specialization: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  feePerConsultation: {
+    type: Number,
+    required: true,
+  },
+  timings: {
+    type: Array,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+}, { timestamps: true });
 
-
-},{timestamps:true})
-
-const doctorModel = mongoose.model("doctors",docSchema);
+const doctorModel = mongoose.model("doctors", docSchema);
 export default doctorModel;
