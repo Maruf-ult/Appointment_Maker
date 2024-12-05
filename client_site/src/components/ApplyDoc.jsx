23ef3d821f224fd6b82af2e8adf8959cba7feee0
maruf_hossain <marufhossain123098@gmail.com>
2024-12-05 22:19:17 +0600

    apply doc completed

--- client_site/src/components/ApplyDoc.jsx
+++ client_site/src/components/ApplyDoc.jsx
@@ -1,11 +1,12 @@
-import  { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { hideLoading, showLoading } from "../Redux/AlertSlice.jsx";
 import ForClients from "./ForClients.jsx";
-import {  TimePicker } from 'antd';
+import { TimePicker } from 'antd';
+import dayjs from 'dayjs';
 
 function ApplyDoc() {
   const dispatch = useDispatch();
@@ -15,15 +16,13 @@ function ApplyDoc() {
   const [formValues, setFormValues] = useState({
     firstName: "",
     lastName: "",
-    email: "",
     phoneNumber: "",
-    image: "",
-    department: "",
-    profession: "",
-    experience: "",
+    website: "",
     address: "",
-    feePerVisit: "",
-    timings: "",
+    specialization: "",
+    experience: "",
+    feePerConsultation: "",
+    timings: [],
   });
 
   const handleInputChange = (e) => {
@@ -31,6 +30,13 @@ function ApplyDoc() {
     setFormValues((prevValues) => ({
       ...prevValues,
       [name]: value,
+    }));
+  };
+
+  const handleTimingsChange = (value) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      timings: value ? [value[0].format('HH:mm'), value[1].format('HH:mm')] : [],
     }));
   };
 
@@ -59,7 +65,16 @@ function ApplyDoc() {
       }
     } catch (error) {
       dispatch(hideLoading());
-      toast.error(error);
+      if (error.response) {
+        toast.error(`Server Error: ${error.response.data.message}`);
+        console.error("Error Response:", error.response.data);
+      } else if (error.request) {
+        toast.error("No response from server. Please try again later.");
+        console.error("Error Request:", error.request);
+      } else {
+        toast.error("Request Error: " + error.message);
+        console.error("Error Message:", error.message);
+      }
     }
   };
 
@@ -67,23 +82,23 @@ function ApplyDoc() {
     <ForClients>
       <h1 className="font-semibold text-black pl-3 pt-1">Apply Doctor Account</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="text-black">
         <div className="flex space-x-12 pl-16 pt-6">
           <div className="space-y-2">
-            <h3 className="text-black">*First Name</h3>
+            <h3 className="text-black ">*First Name</h3>
             <input
               name="firstName"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.firstName}
               onChange={handleInputChange}
             />
           </div>
           <div className="space-y-2">
-            <h3 className="text-black">*Last Name</h3>
+            <h3 className="text-black ">*Last Name</h3>
             <input
               name="lastName"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.lastName}
               onChange={handleInputChange}
@@ -93,7 +108,7 @@ function ApplyDoc() {
             <h3 className="text-black">*Phone Number</h3>
             <input
               name="phoneNumber"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.phoneNumber}
               onChange={handleInputChange}
@@ -106,7 +121,7 @@ function ApplyDoc() {
             <h3 className="text-black">*Website</h3>
             <input
               name="website"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.website}
               onChange={handleInputChange}
@@ -116,20 +131,22 @@ function ApplyDoc() {
             <h3 className="text-black">*Address</h3>
             <input
               name="address"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.address}
               onChange={handleInputChange}
             />
           </div>
         </div>
-          <h1 className="text-black font-semibold ml-3 mt-1">Professional Information</h1>
+
+        <h1 className="text-black font-semibold ml-3 mt-1">Professional Information</h1>
+
         <div className="flex space-x-12 pl-16 pt-2">
           <div className="space-y-2">
             <h3 className="text-black">*Specialization</h3>
             <input
               name="specialization"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.specialization}
               onChange={handleInputChange}
@@ -139,38 +156,35 @@ function ApplyDoc() {
             <h3 className="text-black">*Experience</h3>
             <input
               name="experience"
-              className="px-8 py-1 border border-black bg-slate-200"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
               value={formValues.experience}
               onChange={handleInputChange}
             />
           </div>
           <div className="space-y-2">
-            <h3 className="text-black">*Fee per Cunsultation</h3>
+            <h3 className="text-black">*Fee per Consultation</h3>
             <input
-              name="fee per Cunsultation"
-              className="px-8 py-1 border border-black bg-slate-200"
+              name="feePerConsultation"
+              className="px-8 py-1 border border-black bg-slate-200 font-light"
               type="text"
-              value={formValues.feeperCunsultation}
+              value={formValues.feePerConsultation}
               onChange={handleInputChange}
             />
           </div>
         </div>
-        <div className="flex space-x-12 pl-16 pt-2">
 
+        <div className="flex space-x-12 pl-16 pt-2">
           <div className="space-y-2">
             <h3 className="text-black ">*Timings</h3>
-            {/* <input
-              name="timings"
-              className="px-8 py-1 border border-black bg-slate-200"
-              type="text"
-              value={formValues.timings}
-              onChange={handleInputChange}
-            /> */}
-            <TimePicker.RangePicker className="border border-black py-1 " />
-
+            <TimePicker.RangePicker
+              className="border border-black py-1 font-light"
+              value={formValues.timings.length ? [dayjs(formValues.timings[0], 'HH:mm'), dayjs(formValues.timings[1], 'HH:mm')] : null}
+              onChange={handleTimingsChange}
+            />
           </div>
         </div>
+
         <div className="flex justify-end pr-20 pt-4">
           <button
             type="submit"
